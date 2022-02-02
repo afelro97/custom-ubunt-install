8a7b3b4c25df49afa45b20e6237cad3506bb8128
afelro97 <afelro97@gmail.com>
2022-02-02 09:39:20 -0500

    Desktop environment and android studio

--- ubuntu-script-installer/installer.sh
+++ ubuntu-script-installer/installer.sh
@@ -13,6 +13,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io \
 sudo groupadd docker \
 sudo usermod -aG docker $USER \
 newgrp docker \
+#install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
 sudo chmod +x /usr/local/bin/docker-compose \
 sudo apt update \
@@ -20,4 +21,13 @@ sudo apt install software-properties-common apt-transport-https wget \
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - \
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" \
 sudo apt install code \
-
+#install xfce desktop environment
+sudo apt install lightdm xfce4 \
+sudo cp /ubuntu-xfce-desktop-config/lightdm.conf /etc/lightdm/lightdm.conf \
+#install neovim
+sudo apt-get install neovim \
+#install android studio
+sudo apt install openjdk-11-jdk \
+sudo add-apt-repository ppa:maarten-fonville/android-studio \
+sudo apt update \
+sudo apt install android-studio \
