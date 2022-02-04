fd6237c9e3a8cfe4dba9b97ea1d02ffa915a216b
afelro97 <afelro97@gmail.com>
2022-02-03 20:48:33 -0500

    installer.sh removed

--- installer_v2.sh
+++ installer_v2.sh
@@ -18,6 +18,8 @@ sudo groupadd docker
 
 sudo usermod -aG docker $USER 
 
+sudo newgrp docker
+
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 
 sudo chmod +x /usr/local/bin/docker-compose 
@@ -67,6 +69,9 @@ sudo apt install kdenlive
 sudo add-apt-repository ppa:atareao/telegram
 sudo apt update -y
 sudo apt install telegram -y
+
+#install firefox
+sudo apt install firefox -y
 
 #reboot
 sudo reboot
